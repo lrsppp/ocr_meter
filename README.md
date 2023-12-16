d2a97a7920e5c101dced5798450b3b422a33323f
Lars Poppe <p.larsf@gmail.com>
2023-12-16 15:25:29 +0100

    bypass pytesseract specific hacks, add example to find initial model

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+
+add algorithm.py
+add app.py
+add photo_parser.py (time etc)
+
+
+* Dockerfile
+    - App
+    - MongoDB
+* FastAPI App
+    - Parse Images
--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import re
 import pytesseract
+from deskew import determine_skew
+import math
 
 
 def resize_image(img, size=(1024, 768)):
@@ -19,13 +21,13 @@ def find_meter_rectangle(img):
     :param: Image
     :return: Rectangle, i.e. cropped image
     """
-    gray = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     thresh = cv2.adaptiveThreshold(
         gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 51, 9
     )
 
     # Fill rectangular contours
-    cnts = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
     for c in cnts:
         cv2.drawContours(thresh, [c], -1, (255, 255, 255), -1)
@@ -47,18 +49,52 @@ def find_meter_rectangle(img):
     return img[y : y + h, x : x + w]
 
 
+def rotate(image, angle, background):
+    old_width, old_height = image.shape[:2]
+    angle_radian = math.radians(angle)
+    width = abs(np.sin(angle_radian) * old_height) + abs(
+        np.cos(angle_radian) * old_width
+    )
+    height = abs(np.sin(angle_radian) * old_width) + abs(
+        np.cos(angle_radian) * old_height
+    )
+
+    image_center = tuple(np.array(image.shape[1::-1]) / 2)
+    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+    rot_mat[1, 2] += (width - old_width) / 2
+    rot_mat[0, 2] += (height - old_height) / 2
+    return cv2.warpAffine(
+        image, rot_mat, (int(round(height)), int(round(width))), borderValue=background
+    )
+
+
+def deskew_image(img):
+    grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    angle = determine_skew(grayscale)
+    rotated = rotate(img, angle, (0, 0, 0))
+    return rotated
+
+
 def morph_rect(rect, kernel_size=(3, 3)):
     """
     Preprocess rectangle (cropped image) using thresholding and morphological
     transformations.
 
+    See also: https://tesseract-ocr.github.io/tessdoc/ImproveQuality.html
+
     :param rect: Rectangle
     :param kernel_size: Kernel size passed to `cv2.morphologyEx(..., kernel_size)`
     :return: Transformed rectangle
     """
-    gray = cv2.cvtColor(rect, cv2.COLOR_BGRA2GRAY)
-    _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)
-    morph_close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel_size)
+    (h, w) = rect.shape[:2]
+    rect = cv2.resize(rect, (w * 32, h * 32))
+    gray = cv2.cvtColor(rect, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (0, 0), sigmaX=10, sigmaY=10)
+    gray = cv2.erode(gray, None, iterations=20)
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)[1]
+    # thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    morph_close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, (15, 15))
+    morph_close = cv2.bitwise_not(morph_close)
     return morph_close
 
 
@@ -71,11 +107,13 @@ def parse_rect(rect, custom_config=None):
     :return: Number
     """
     if custom_config is None:
-        custom_config = r"--oem 3 --psm 11 -c tessedit_char_whitelist=0123456789"
+        custom_config = (
+            r"--oem 1 --psm 13 -c tessedit_char_whitelist=0123456789,"  # or --psm 7
+        )
     result = pytesseract.image_to_string(rect, config=custom_config)
     cleaned_result = re.sub(r"[^\d,\.]", "", result)
     try:
         num = float(cleaned_result)
     except:
         num = np.nan
-    return num
+    return cleaned_result
